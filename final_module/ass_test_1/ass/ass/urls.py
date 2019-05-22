579f2d8df2a5be8a37718a7d3a2d71cfba5cc9ea
Abedeljalil Jarjanazy <abode-j@hotmail.com>
2019-05-22 14:50:41 +0300

    add entity view is working, started working on the delete button

--- final_module/ass_test_1/ass/ass/urls.py
+++ final_module/ass_test_1/ass/ass/urls.py
@@ -4,7 +4,7 @@ from django.views.generic.base import RedirectView
 
 
 # import views
-from main.views import sign_in_view, admin_home_page_view, create_admin_view, client_home_page_view, entity_detail_view, signout_view, add_entities_view
+from main.views import *
 
 favicon_view = RedirectView.as_view(url='main/static/favicon.ico', permanent=True)
 
@@ -20,11 +20,14 @@ urlpatterns = [
     path('admin_homepage/', admin_home_page_view, name='adminHomePage'),
     path('admin_homepage/details/', entity_detail_view, name='adminEntityDetailsPage'),
     path('admin_homepage/add_entity/', add_entities_view, name='adminAddEntityPage'),
+    path('admin_homepage/update_entity/', add_entities_view, name='adminUpdateEntityPage'),
+    path('admin_homepage/delete_entity/', delete_entities_view, name='adminDeleteEntityPage'),
 
     #client paths
     path('client_homepage/', client_home_page_view, name='clientHomePage'),
     path('client_homepage/details/', entity_detail_view, name='clientEntityDetailsPage'),
-    path('client_homepage/add_entity/', add_entities_view, name='clientAddEntityPage'),
+    path('client_homepage/update_entity/', add_entities_view, name='clientUpdateEntityPage'),
+    path('client_homepage/delete_entity/', delete_entities_view, name='clientDeleteEntityPage'),
 
     #common paths
 
--- final_module/ass_test_1/ass/main/forms.py
+++ final_module/ass_test_1/ass/main/forms.py
@@ -2,14 +2,46 @@ from django import forms
 
 from main.models import *
 
-class create_admin_form(forms.Form):
-    username = forms.CharField(
-        widget=forms.TextInput(attrs={'placeholder': 'the username',})
-    )
 
-    password = forms.CharField(
-        widget=forms.PasswordInput()
-    )
+class instructorForm(forms.ModelForm):
+
+    class Meta:
+        model = Instructor
+        fields = ['name', 'surname', 'dept_code',]
 
 
+class departmentForm(forms.ModelForm):
 
+    class Meta:
+        model = Department
+        fields = ['code', 'name',]
+
+class curriculumForm(forms.ModelForm):
+
+    class Meta:
+        model = Curriculum
+        fields = ['version', 'dept_code',]
+
+class courseForm(forms.ModelForm):
+
+    class Meta:
+        model = Course
+        fields = ['code', 'name', 'credit']
+
+class courseofferingForm(forms.ModelForm):
+
+    class Meta:
+        model = Courseoffering
+        fields = ['semester', 'course_code']
+
+class studentForm(forms.ModelForm):
+
+    class Meta:
+        model = Student
+        fields = ['id', 'name', 'surname', 'dep_code']
+
+class semesterForm(forms.ModelForm):
+
+    class Meta:
+        model = Semester
+        fields = ['type', 'year']
